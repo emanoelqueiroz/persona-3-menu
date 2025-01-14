1563314be2c1b16b1292a0c1609080c391ac04db
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2025-01-14 03:06:05 -0300

    dynamic links, fixed animations and added bottom actions and descriptions

--- script.js
+++ script.js
@@ -1,13 +1,73 @@
 const body = document.querySelector('body')
 const menuWrapper = document.querySelector('.menu-wrapper')
-const moneyBox = document.querySelector('.money-box')
+const menu = menuWrapper.querySelector('.menu')
 const videoWrapper = document.querySelector('.video-wrapper')
 const video = document.querySelector('#video')
-const itens = menuWrapper.querySelectorAll('li')
 const welcomeWrapper = document.querySelector('.welcome-wrapper')
+const itemDescription = document.querySelector('.item-description')
+const main = document.querySelector('main')
 
 const audio = new Audio('sounds/menu.mp3')
 const bgMusic = new Audio('sounds/color-your-night.mp3')
+
+const MENU_LINKS = [
+    {
+        title: 'Skill',
+        description: 'Use a Skill',
+        opacity: 1,
+        rotation: '-10deg',
+    },
+    {
+        title: 'Item',
+        description: 'View/Use Items',
+        opacity: 0.8,
+        rotation: '-5deg',
+    },
+    {
+        title: 'Equip',
+        description: 'View/Change Equipment',
+        opacity: 0.6,
+        rotation: '-10deg',
+    },
+    {
+        title: 'Persona',
+        description: 'View/Change Persona',
+        opacity: 1,
+        rotation: '-5deg',
+    },
+    {
+        title: 'Stats',
+        description: 'View Stats/Organize Party',
+        opacity: 0.8,
+        rotation: '-10deg',
+    },
+    {
+        title: 'Quest',
+        description: 'View Requests',
+        opacity: 0.6,
+        rotation: '-5deg',
+    },
+    {
+        title: 'Social Link',
+        description: 'View Social Links',
+        opacity: 1,
+        rotation: '-10deg',
+    },
+    {
+        title: 'Calendar',
+        description: 'View Calendar',
+        opacity: 0.8,
+        rotation: '-5deg',
+    },
+    {
+        title: 'System',
+        description: 'View Settings',
+        opacity: 0.6,
+        rotation: '-10deg',
+    },
+]
+
+let initiated = false
 
 bgMusic.volume = 0.4
 
@@ -21,30 +81,58 @@ body.addEventListener('click', () => {
 
 bgMusic.addEventListener('ended', () => bgMusic.play())
 
-itens.forEach(item => {
-    item.addEventListener('mouseover', (e) => {
-        if (e.target.classList.contains('selected')) {
-            return
+function insertListItens() {
+    const html = MENU_LINKS.reduce((acc, item, currentIndex) => {
+        let className = ''
+
+        if (currentIndex === 0) {
+            className = 'class="selected"'
         }
 
-        audio.pause()
-        audio.currentTime = 0
+        acc += `<li style="transform: rotate(${item.rotation}); opacity: ${item.opacity};" ${className} data-description="${item.description}">${item.title}</li>`
 
-        itens.forEach(item => item.classList.remove('selected'))
-        e.target.classList.add('selected')
+        return acc
+    }, '')
 
-        audio.play()
+    menu.innerHTML = html
+}
+
+function initMenuLinkOver() {
+    const itens = menu.querySelectorAll('li')
+
+    itens.forEach(item => {
+        item.addEventListener('mouseover', (e) => {
+            if (e.target.classList.contains('selected')) {
+                return
+            }
+
+            itemDescription.innerHTML = e.target.dataset?.description || ''
+
+            audio.pause()
+            audio.currentTime = 0
+
+            itens.forEach(item => item.classList.remove('selected'))
+            e.target.classList.add('selected')
+
+            audio.play()
+        })
     })
-})
+}
 
-video.addEventListener("timeupdate", (e) => {
-    if (video.currentTime > 1.60) {
-        body.classList.add('background')
-        moneyBox.classList.add('show')
-        menuWrapper.classList.add('show')
+video.addEventListener('timeupdate', () => {
+    if (video.currentTime < 1.60 || initiated) {
+        return
     }
+
+    initiated = true
+
+    body.classList.add('background')
+    main.classList.add('show')
 })
 
-video.addEventListener("ended", () => {
+video.addEventListener('ended', () => {
     videoWrapper.innerHTML = '<video autoplay muted loop id="video"><source src="videos/menu-2.mp4" type="video/mp4"></video>';
 })
+
+insertListItens()
+initMenuLinkOver()
